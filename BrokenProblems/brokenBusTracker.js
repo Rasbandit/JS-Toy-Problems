1ceff00dd24cfcf0ae52678977bf253ea2f737cd
Travis Allen <travisallen6@gmail.com>
2018-06-27 15:02:32 -0600

    Clarification in the bus tracker instructions

--- BrokenProblems/brokenBusTracker.js
+++ BrokenProblems/brokenBusTracker.js
@@ -1,14 +1,14 @@
 // Number of people in the bus
 //
-// There is a bus moving in the city, and it takes and drop some people in each bus stop.
+// There is a bus moving in the city, and it picks up and drops off some people at each bus stop.
 //
-// You are provided a list (or array in JS) of integer array. Each integer array has two items which represent number of people get into bus (The first item) and number of people get off the bus (The second item).
+// You are provided an array of integer arrays. Each integer array has two items which represent number of people that the bus picks up (The first item) and number of people that the bus drops off (The second item).
 //
-// The first integer array has 0 number in the second item, since the bus is empty in the first bus stop.
+// The first integer array has the number 0 in the second item, since the bus is empty in the first bus stop.
 //
-// Your task is to return number of people who are still in the bus after the last bus station. Even though it is the last stop, some people don't get off the bus, and they are probably sleeping there :D
+// Your task is to return the number of people who are still in the bus after the last bus station. Even though it is the last stop, some people don't get off the bus, and they are probably sleeping there :D
 //
-// Take a look on the test cases.
+// Take a look at the test cases.
 //
 // Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
 
