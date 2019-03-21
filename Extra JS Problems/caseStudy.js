1e3a95892ea575907709aaa9ebccaa8eb92064d1
Payton Whipple <web-dev@paytonwhipple.com>
2019-03-21 13:21:16 -0600

    fix spelling errors

--- Extra JS Problems/caseStudy.js
+++ Extra JS Problems/caseStudy.js
@@ -1,13 +1,13 @@
 // A case study for string manipulation.
 // In programing we have diffrent nameing schemes called cases.
 // Each case has a specific way of being written and diffrent use cases.
-// camalCase - no spaces words all exept the first word are capitalized (this is the default for Javascript).
+// camelCase - no spaces words all exept the first word are capitalized (this is the default for Javascript).
 // snake_case - all lower case, words are seperated by an underscore.
 // kabab-case - all lower case, words are seperated by a dash.
 // the problem is to create a function that takes in a string and converts it into an object that has properties
 //	for each of these cases as well as the original with the value being the string converted into that case.
 // eg. caseConverter("Hey guys") => {
-//	camalCase: "heyGuys",
+//	camelCase: "heyGuys",
 //	snakeCase: "hey_guys",
 //	kababCase: "hey-guys",
 //	original: "Hey Guys"
@@ -16,11 +16,11 @@
 
 // To todd: this is optional to tone down the difficulty if the structure is wierd
 function caseConverter(string) {
-	let camalCase;
-	let camalCase;
-	let camalCase;
+	let camelCase;
+	let snakeCase;
+	let kababCase;
 	return {
-		camalCase,
+		camelCase,
 		snakeCase,
 		kababCase,
 		original: string
